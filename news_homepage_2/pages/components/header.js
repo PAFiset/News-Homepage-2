bb5ce01c32f5d2359397b382408f8f4875435d66
PAFiset <114942984+PAFiset@users.noreply.github.com>
2023-04-20 13:46:14 -0400

    new modal animation

--- news_homepage_2/pages/components/header.js
+++ news_homepage_2/pages/components/header.js
@@ -1,11 +1,10 @@
-import { useState , useEffect } from 'react';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Header = () => {
+  const [isMobileModal, setMobileModal] = useState(false);
 
-  const [isMobileModal,setMobileModal] = useState(false);
-
-  const getWindowDimension = () =>{
+  const getWindowDimension = () => {
     const width = window.innerWidth;
     return width;
   };
@@ -17,45 +16,50 @@ const Header = () => {
   useEffect(() => {
     const links = document.querySelectorAll(".links");
     console.log(` render +  ${links}`);
-    
+
     function handleResize() {
       getWindowDimension() < 700 ? "" : setMobileModal(false);
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     links.forEach((link) => {
-      link.addEventListener("click",function () {
-        setMobileModal(false)
+      link.addEventListener("click", function () {
+        setMobileModal(false);
       });
     });
 
-    return() => {
-      window.removeEventListener("resize",handleResize);
-    }
-
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
-
-  return ( 
-    <header >    
-      <nav className="navbar" >
-          <Link href="/"><img src="./images/logo.svg" alt="W. logo"/></Link>
-          <div className={isMobileModal ? "navcontent" : "navcontent" + " " + "navbarmobileclosed"}>
-              <Link href="/" >Home</Link>
-              <Link href="/" >New</Link>
-              <Link href="/" >Popular</Link>
-              <Link href="/" >Trending</Link>
-              <Link href="/" >Categories</Link>
-          </div>
-      </nav> 
-      <button className={ isMobileModal ? "navicon_open" + " " + "navicon" : "navicon"} onClick={clickHandler} >
-          <span></span>
-          <span></span>
-          <span></span>
+  return (
+    <header>
+      <nav className="navbar">
+        <Link href="/">
+          <img src="./images/logo.svg" alt="W. logo" />
+        </Link>
+        <div
+          className={isMobileModal ? "navcontent open" : "navcontent closed "}
+        >
+          <Link href="/">Home</Link>
+          <Link href="/">New</Link>
+          <Link href="/">Popular</Link>
+          <Link href="/">Trending</Link>
+          <Link href="/">Categories</Link>
+        </div>
+      </nav>
+      <button
+        className={isMobileModal ? "navicon_open" + " " + "navicon" : "navicon"}
+        onClick={clickHandler}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </button>
     </header>
   );
-}
- 
+};
+
 export default Header;
